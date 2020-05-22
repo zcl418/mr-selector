31350d833cf4c31e817ca63e55b5493fe01ce5f1
夏雨的歌 <zcl418@qq.com>
2020-05-22 15:36:11 +0800

    first update

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const { dialog } = require('electron').remote
+const fs = require('fs')
+window.onload = function () {
+    const btn_select = document.querySelector("#btn-select-file");
+    const btn_read = document.querySelector("#btn-read-file");
+    const file_path = document.querySelector("#file-path");
+    const container = document.querySelector("#container");
+    let filePath = "";
+
+    // get file path
+    btn_select.onclick = () => {
+        dialog.showOpenDialog({
+            filters: [
+                { name: 'data', extensions: ['csv'] },
+            ]
+        }).then((result) => {
+            if (!result.canceled) {
+                filePath = result.filePaths[0];
+                file_path.innerHTML = filePath;
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    btn_read.onclick = () => {
+        if (filePath != "") {
+            let stats = fs.statSync(filePath)
+             // 和主进程通信
+            const { ipcRenderer } = require('electron');
+            ipcRenderer.send("process",[stats.size,filePath]);
+            ipcRenderer.on('progress',(event,arg)=>{
+                container.innerHTML = arg; 
+            })
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,75 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const readline = require('readline')
+const fs = require('fs')
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+// app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow();
+})
+
+app.allowRendererProcessReuse = false;
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+// =========================
+
+const { ipcMain } = require('electron')
+ipcMain.on('process', (event, arg) => {
+  console.log(arg)
+  
+  let reader = readline.createInterface({
+    input: fs.createReadStream(arg[1],{encoding:'utf8'})
+  })
+
+  let i = 0
+  let size = 0
+  // 一行一行地读取文件
+  reader.on('line', function (line) {
+    i++;
+    size += Buffer.byteLength(line)
+    if(i % 100000 == 0){
+      console.log(line.split(','))
+      
+      console.log(i," - ",size,"/",arg[0])
+      event.reply('progress',size / arg[0])
+    }
+  });
+  // 读取完成后,将arr作为参数传给回调函数
+  reader.on('close', function () {
+    // container.innerHTML = "完成！"
+    console.log(i,"完成！")
+  });
+})
