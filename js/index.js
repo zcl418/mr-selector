7c726571e41277b2740a5579d5327e7b4dd45770
bryce <zcl418@qq.com>
2020-06-10 19:40:28 +0800

    ui change

--- js/index.js
+++ js/index.js
@@ -1,37 +1,64 @@
-const { dialog } = nodeRequire('electron').remote
-const fs = nodeRequire('fs')
+const { dialog } = nodeRequire('electron').remote;
+const fs = nodeRequire('fs');
+const readline = nodeRequire('readline');
 window.onload = function () {
-    const btn_select = document.querySelector("#btn-select-file");
-    const btn_read = document.querySelector("#btn-read-file");
-    const file_path = document.querySelector("#file-path");
-    const container = document.querySelector("#container");
-    let filePath = "";
+  const btn_select = document.querySelector('#btn-select-file');
+  const btn_read = document.querySelector('#btn-read-file');
+  const file_path = document.querySelector('#file-path');
+  const container = document.querySelector('#container');
+  let filePath = '';
 
-    // get file path
-    btn_select.onclick = () => {
-        dialog.showOpenDialog({
-            filters: [
-                { name: 'data', extensions: ['csv'] },
-            ]
-        }).then((result) => {
-            if (!result.canceled) {
-                filePath = result.filePaths[0];
-                file_path.value = filePath;
-            }
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-
-    btn_read.onclick = () => {
-        if (filePath != "") {
-            let stats = fs.statSync(filePath)
-             // 和主进程通信
-            const { ipcRenderer } = require('electron');
-            ipcRenderer.send("process",[stats.size,filePath]);
-            ipcRenderer.on('progress',(event,arg)=>{
-                container.innerHTML = arg; 
-            })
+  // 获取文件路径
+  btn_select.onclick = () => {
+    dialog
+      .showOpenDialog({
+        filters: [{ name: 'data', extensions: ['csv'] }],
+      })
+      .then((result) => {
+        if (!result.canceled) {
+          filePath = result.filePaths[0];
+          file_path.value = filePath;
+          read_first_line(filePath);
         }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // 单击运行按钮
+  btn_read.onclick = () => {
+    if (filePath != '') {
+      let stats = fs.statSync(filePath);
+      // 和主进程通信
+      const { ipcRenderer } = nodeRequire('electron');
+      ipcRenderer.send('process', [stats.size, filePath]);
+      ipcRenderer.on('progress', (event, arg) => {
+        container.innerHTML = arg;
+      });
     }
+  };
+};
+
+// 读取第一行
+function read_first_line(path) {
+  let reader = readline.createInterface({
+    input: fs.createReadStream(path, { encoding: 'utf8' }),
+  });
+  let rs = null;
+  let rs2 = [];
+  reader.on('line', function (line) {
+    rs = line.split(',');
+    rs.forEach((el) => {
+      console.log(remove_quotes(el) + '--');
+    });
+    reader.close();
+  });
+  // reader.on('close', () => {
+  //   console.log(rs);
+  // });
+}
+
+function remove_quotes(str) {
+  str.replace(/["]/g, '');
 }
--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const readline = require('readline')
-const fs = require('fs')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const readline = require('readline');
+const fs = require('fs');
 
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 600,
+    height: 400,
+    // resizable: false,
+    autoHideMenuBar: true,
+    icon: 'Dragonfly_32px.ico',
+    title: 'MR数据筛选工具',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
+
+  // Menu.setApplicationMenu(null);
 }
 
 // This method will be called when Electron has finished
@@ -28,48 +34,50 @@ function createWindow() {
 // app.whenReady().then(createWindow)
 app.whenReady().then(() => {
   createWindow();
-})
+});
 
 app.allowRendererProcessReuse = false;
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
 
 // =========================
 
-const { ipcMain } = require('electron')
+const { ipcMain } = require('electron');
+// 读取数据时处理
 ipcMain.on('process', (event, arg) => {
-  console.log(arg)
-  
-  let reader = readline.createInterface({
-    input: fs.createReadStream(arg[1],{encoding:'utf8'})
-  })
+  console.log(arg);
 
-  let i = 0
-  let size = 0
+  let reader = readline.createInterface({
+    input: fs.createReadStream(arg[1], { encoding: 'utf8' }),
+  });
+
+  let i = 0;
+  let size = 0;
   // 一行一行地读取文件
   reader.on('line', function (line) {
     i++;
-    size += Buffer.byteLength(line)
-    if(i % 100000 == 0){
-      console.log(line.split(','))
-      
-      console.log(i," - ",size,"/",arg[0])
-      event.reply('progress',size / arg[0])
+    size += Buffer.byteLength(line);
+    if (i % 100000 == 0) {
+      console.log(line.split(','));
+      console.log(i, ' - ', size, '/', arg[0]);
+      event.reply('progress', size / arg[0]);
+      reader.close();
     }
   });
   // 读取完成后,将arr作为参数传给回调函数
   reader.on('close', function () {
     // container.innerHTML = "完成！"
-    console.log(i,"完成！")
+    // console.log(i, '完成！');
+    event.reply('progress', '完成');
   });
-})
+});
